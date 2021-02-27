af584b59af5a6f27c329b3e3f29b3760fba600a9
Amanshu Raikwar <amanshuraikwar.in@gmail.com>
2021-02-27 17:29:10 +0800

    Sample data

--- app/src/main/java/com/example/androiddevchallenge/model/Puppy.kt
+++ app/src/main/java/com/example/androiddevchallenge/model/Puppy.kt
@@ -36,36 +36,91 @@ data class Puppy(
     }
 
     companion object {
-        private val imageList = listOf(
-            "https://images.unsplash.com/photo-1546238232-20216dec9f72?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwyMTQ2N3wwfDF8c2VhcmNofDF8fHB1cHBpZXN8ZW58MHx8fA&ixlib=rb-1.2.1&q=80&w=1080",
-            "https://images.unsplash.com/photo-1519150268069-c094cfc0b3c8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwyMTQ2N3wwfDF8c2VhcmNofDJ8fHB1cHBpZXN8ZW58MHx8fA&ixlib=rb-1.2.1&q=80&w=1080",
-            "https://images.unsplash.com/photo-1560807707-8cc77767d783?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwyMTQ2N3wwfDF8c2VhcmNofDN8fHB1cHBpZXN8ZW58MHx8fA&ixlib=rb-1.2.1&q=80&w=1080",
-            "https://images.unsplash.com/photo-1525253086316-d0c936c814f8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwyMTQ2N3wwfDF8c2VhcmNofDR8fHB1cHBpZXN8ZW58MHx8fA&ixlib=rb-1.2.1&q=80&w=1080",
-            "https://images.unsplash.com/photo-1610112645245-36020fc0e128?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwyMTQ2N3wwfDF8c2VhcmNofDV8fHB1cHBpZXN8ZW58MHx8fA&ixlib=rb-1.2.1&q=80&w=1080",
-            "https://images.unsplash.com/photo-1548199973-03cce0bbc87b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwyMTQ2N3wwfDF8c2VhcmNofDZ8fHB1cHBpZXN8ZW58MHx8fA&ixlib=rb-1.2.1&q=80&w=1080",
-            "https://images.unsplash.com/photo-1583336663277-620dc1996580?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwyMTQ2N3wwfDF8c2VhcmNofDd8fHB1cHBpZXN8ZW58MHx8fA&ixlib=rb-1.2.1&q=80&w=1080",
-            "https://images.unsplash.com/photo-1592817797597-392e3b878e1c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwyMTQ2N3wwfDF8c2VhcmNofDh8fHB1cHBpZXN8ZW58MHx8fA&ixlib=rb-1.2.1&q=80&w=1080",
-            "https://images.unsplash.com/photo-1548658166-136d9f6a7e76?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwyMTQ2N3wwfDF8c2VhcmNofDl8fHB1cHBpZXN8ZW58MHx8fA&ixlib=rb-1.2.1&q=80&w=1080"
+        private val puppyPhotos by lazy {
+            fun String.url(): String {
+                return "https://images.unsplash.com/$this?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwyMTQ2N3wwfDF8c2VhcmNofDF8fHB1cHBpZXN8ZW58MHx8fA&ixlib=rb-1.2.1&q=80&w=1080"
+            }
+
+            listOf(
+                listOf(
+                    "photo-1560807707-8cc77767d783".url(),
+                    "photo-1593134257782-e89567b7718a".url(),
+                ),
+                listOf(
+                    "photo-1591160690555-5debfba289f0".url(),
+                    "photo-1507146426996-ef05306b995a".url(),
+                    "photo-1600804340584-c7db2eacf0bf".url(),
+                ),
+                listOf(
+                    "photo-1610621488354-c7274b9690d1".url(),
+                    "photo-1577198499139-620a2f90f668".url(),
+                    "photo-1589941013453-ec89f33b5e95".url(),
+                    "photo-1612389648232-d506dd957ca3".url(),
+                ),
+                listOf(
+                    "photo-1576415928601-b25522bc9e5a".url(),
+                    "photo-1561754050-9a1ee0470c73".url(),
+                    "photo-1586713471521-2325b2b674e4".url(),
+                    "photo-1509463545580-61e747a6a12f".url(),
+                ),
+                listOf(
+                    "photo-1575535468632-345892291673".url(),
+                    "photo-1604891599626-9bc48fafb832".url(),
+                    "photo-1523171067267-bb96f975c4bb".url(),
+                ),
+            )
+        }
+
+        private val breeds by lazy {
+            listOf(
+                "Cavalier King",
+                "Golden Retriever",
+                "German Shepherd",
+                "Bull Dog",
+                "Shiba Inu",
+            )
+        }
+
+        private val dogNames = listOf(
+            "Luna",
+            "Duke",
+            "Rocky",
+            "Milo",
+            "Jackson",
+            "Maddie",
+            "Buster",
+            "Frankie",
+            "Apollo",
         )
 
-        operator fun invoke() = Puppy(
-            name = "Mr. Peabody",
-            breed = "Golder Retriever",
-            puppyPhotoUrlList = listOf(
-                imageList.random(),
-                imageList.random(),
-                imageList.random(),
-                imageList.random()
-            ),
-            age = 3,
-            gender = Gender.Boi,
-            kmsAway = 4,
-            location = "Bay Area, San Francisco",
-            bio = "Foxtrot is a 6-year-old, house trained, " +
-                "male Mix Breed that is super friendly. " +
-                "Loves people, other dogs, even cats. " +
-                "He loves playing with his dog toys and going on walks. " +
-                "He loves to be with his people, and explore!"
-        )
+        operator fun invoke(): Puppy {
+            val index = (0..3).random()
+            val name = dogNames.random()
+            val age = (1..10).random()
+            val gender = if ((0..1).random() == 0) {
+                Gender.Gurl
+            } else {
+                Gender.Boi
+            }
+            val pronoun = if (gender == Gender.Boi) {
+                "He"
+            } else {
+                "She"
+            }
+            return Puppy(
+                name = name,
+                breed = breeds[index],
+                puppyPhotoUrlList = puppyPhotos[index],
+                age = age,
+                gender = gender,
+                kmsAway = (0..10).random(),
+                location = "Toa Payoh",
+                bio = "$name is a $age year old house trained " +
+                        "$gender. $pronoun is super friendly. " +
+                        "$pronoun loves people, other dogs, even cats. " +
+                        "$pronoun loves playing with toys and going on walks. " +
+                        "$pronoun loves to explore!"
+            )
+        }
     }
 }
